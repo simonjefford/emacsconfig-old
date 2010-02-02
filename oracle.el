c26af69ce5da45c96c5114f2b0f0f01bd9fe472e
Simon Jefford <simon.jefford@gmail.com>
2010-02-02 20:48:29 +0000

    Starting from scratch
    First off, the basics + a version of magit that seems to work.

---
+++ oracle.el
@@ -0,0 +1,5 @@
+(add-to-list 'load-path "/usr/local/lib/erlang/lib/tools-2.6.4/emacs")
+(require 'erlang-start)
+
+(set-frame-position (selected-frame) 800 100)
+(set-frame-size (selected-frame) 150 70)
---
+++ simon.el
@@ -0,0 +1,8 @@
+(setq mac-command-modifier 'meta)
+(setq visible-bell nil)
+(if (eq system-type 'darwin)
+    (set-face-font 'default "-apple-Menlo-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+  (set-face-font 'default "Inconsolata 11"))
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-c\C-k" 'kill-region)
