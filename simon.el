547a74839eda1fb4cfe42b36e8a1543bbb16fe48
Simon Jefford <simon.jefford@gmail.com>
2010-02-02 22:42:45 +0000

    More customisation and textmate mode!

--- simon.el
+++ simon.el
@@ -1,8 +1,16 @@
-(setq mac-command-modifier 'meta)
+;(setq mac-command-modifier 'meta)
 (setq visible-bell nil)
+
 (if (eq system-type 'darwin)
     (set-face-font 'default "-apple-Menlo-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1")
   (set-face-font 'default "Inconsolata 11"))
+
 (global-set-key "\C-w" 'backward-kill-word)
 (global-set-key "\C-x\C-k" 'kill-region)
 (global-set-key "\C-c\C-k" 'kill-region)
+
+(ansi-color-for-comint-mode-on)
+
+(add-to-list 'load-path (concat dotfiles-dir "/vendor"))
+(require 'textmate)
+(textmate-mode)
