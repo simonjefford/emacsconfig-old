ffa99f134fb6227b7ebefc17f66b5389cfc96da4
Simon Jefford <simon.jefford@gmail.com>
2010-03-17 17:14:46 +0000

    Upgrade org mode

--- simon.el
+++ simon.el
@@ -21,6 +21,13 @@
 (require 'color-theme)
 (color-theme-initialize)
 (color-theme-deep-blue)
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/org-6.34c/lisp"))
+
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
 
 (require 'edit-server)
 (edit-server-start)
