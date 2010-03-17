3be42c282f2a6b819dc8492ce3f8a28e11c7bbad
Simon Jefford <simon.jefford@gmail.com>
2010-03-17 17:13:39 +0000

    ack-emacs

--- simon.el
+++ simon.el
@@ -25,3 +25,26 @@
 (require 'edit-server)
 (edit-server-start)
 (idle-highlight)
+(require 'ack-emacs)
+
+(defun ack-in-project (pattern)
+  "Run ack, with user-specified ARGS, and collect output in a buffer.
+While ack runs asynchronously, you can use the \\[next-error] command to
+find the text that ack hits refer to. The command actually run is
+defined by the ack-command variable."
+  (interactive (list (read-string "Ack for (in app root): " (thing-at-point 'symbol))))
+ 
+  (let (compile-command
+        (compilation-error-regexp-alist grep-regexp-alist)
+        (compilation-directory default-directory)
+        (ack-full-buffer-name (concat "*ack-" pattern "*")))
+
+    ;; lambda defined here since compilation-start expects to call a function to get the buffer name
+    (compilation-start (concat ack-command " -i  --noheading --nocolor " pattern " " (rinari-root)) 'ack-mode
+                       (when ack-use-search-in-buffer-name
+                         (function (lambda (ignore)
+                                     ack-full-buffer-name)))
+                       (regexp-quote pattern))))
+
+(global-set-key "\C-cfa" 'ack-in-project)
+
