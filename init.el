232cfd2edab3240ddfee5fc22cee8c88cca41b05
Simon Jefford <simon.jefford@gmail.com>
2010-04-30 12:47:33 +0100

    Load the system specific config after the user one
    It relies on stuff in the user specific one

--- init.el
+++ init.el
@@ -70,9 +70,9 @@
       user-specific-dir (concat dotfiles-dir user-login-name))
 (add-to-list 'load-path user-specific-dir)
 
-(if (file-exists-p system-specific-config) (load system-specific-config))
 (if (file-exists-p user-specific-config) (load user-specific-config))
 (if (file-exists-p user-specific-dir)
   (mapc #'load (directory-files user-specific-dir nil ".*el$")))
+(if (file-exists-p system-specific-config) (load system-specific-config))
 
 ;;; init.el ends here
