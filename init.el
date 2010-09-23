8d04d2b03b00e46bb11e8e936cc84f86e4b43169
Simon Jefford <simon.jefford@gmail.com>
2010-09-23 22:04:19 +0100

    Load system-type specific config files

--- init.el
+++ init.el
@@ -67,10 +67,12 @@
 ;; You can keep system- or user-specific customizations here
 (setq system-specific-config (concat dotfiles-dir system-name ".el")
       user-specific-config (concat dotfiles-dir user-login-name ".el")
+      platform-specific-config (concat dotfiles-dir (symbol-name system-type) ".el")
       user-specific-dir (concat dotfiles-dir user-login-name))
 (add-to-list 'load-path user-specific-dir)
 
 (if (file-exists-p user-specific-config) (load user-specific-config))
+(if (file-exists-p platform-specific-config) (load platform-specific-config))
 (if (file-exists-p user-specific-dir)
   (mapc #'load (directory-files user-specific-dir nil ".*el$")))
 (if (file-exists-p system-specific-config) (load system-specific-config))
