b7b6fe60452a8b8eccfea85ce74ef5ea5cb5ca49
Simon Jefford <simon.jefford@gmail.com>
2011-01-12 12:31:37 +0000

    Disable fly-spell mode in org-mode

--- simon.el
+++ simon.el
@@ -29,6 +29,7 @@
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
+(add-hook 'org-mode-hook #'(lambda () (flyspell-mode -1)))
 
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/coffee-mode"))
 (require 'coffee-mode)
