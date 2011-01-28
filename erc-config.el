07260dd1842e38ae2ddf2156fd96c4ed73fe8798
Simon Jefford <simon.jefford@gmail.com>
2011-01-28 13:08:20 +0000

    Implemented a "conference" mode for ERC

--- erc-config.el
+++ erc-config.el
@@ -5,6 +5,18 @@
     (load "~/.emacs.d/erc-credentials.el")
   (message "Couldn't load credentials"))
 
+(defun erc-enable-conference-mode ()
+  (interactive)
+  (setq erc-hide-list (quote ("JOIN" "QUIT" "MODE")))
+  (setq erc-minibuffer-ignored t)
+  (message "Now ignoring JOINs, QUITs, and MODEs"))
+
+(defun erc-disable-conference-mode ()
+  (interactive)
+  (setq erc-hide-list nil)
+  (setq erc-minibuffer-ignored nil)
+  (message "Now showing everything"))
+
 (global-set-key "\C-c\C-e" (lambda () (interactive)
                              (erc :server "irc.freenode.net"
                                   :port "6667"
