626f5beeba2a7cc8e80aeda27193bdaa1b60de98
Simon Jefford <simon.jefford@gmail.com>
2011-04-30 10:11:49 +0100

    Check for graphics mode before setting font etc

--- darwin.el
+++ darwin.el
@@ -1,8 +1,8 @@
-(global-set-key "\M-3" "#")
-(set-face-font 'default "-apple-Menlo-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+(if (display-graphic-p)
+    (progn
+      (global-set-key "\M-3" "#")
+      (set-face-font 'default "-apple-Menlo-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1")))
 
 (load "growl.el")
-
 (setq ecb-source-path (quote ("~/code/rails" "~/code/redis" "~/code/MyJob" "~/.emacs.d" "~/code/delivery_manager")))
 
-(server-start)
