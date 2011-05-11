040304c6af53f4db94e4480e29c4e90e646586ea
Simon Jefford <simon.jefford@gmail.com>
2011-05-11 08:43:53 +0100

    Guardfiles are Ruby

--- starter-kit-ruby.el
+++ starter-kit-ruby.el
@@ -23,6 +23,7 @@
 (add-to-list 'auto-mode-alist '("\\.rjs$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
 
 ;; We never want to edit Rubinius bytecode
 (add-to-list 'completion-ignored-extensions ".rbc")
