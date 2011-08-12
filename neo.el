3d1821e5ed8c2746b238f8aaffc4f9842fce4b7b
Simon Jefford <simon.jefford@gmail.com>
2011-08-12 19:01:57 +0100

    Erlang upgrade on laptop

--- neo.el
+++ neo.el
@@ -1,4 +1,4 @@
-(add-to-list 'load-path "/usr/local/lib/erlang/lib/tools-2.6.6.2/emacs")
+(add-to-list 'load-path "/usr/local/lib/erlang/lib/tools-2.6.6.4/emacs")
 (setq erlang-root-dir "/usr/local/lib/erlang")
 (require 'erlang-start)
 (display-battery-mode)
