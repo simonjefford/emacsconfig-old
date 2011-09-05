797f49e65cdb0115ffd6deb2f72b124384118705
Simon Jefford <simon.jefford@gmail.com>
2011-09-05 12:35:51 +0100

    gtk-ide colour theme

--- simon.el
+++ simon.el
@@ -25,7 +25,7 @@
       (color-theme-initialize)
       (require 'color-theme-ir-black)
       (require 'color-theme-solarized)
-      (color-theme-solarized-dark)))
+      (color-theme-gtk-ide)))
 
 ;; (add-to-list 'load-path (concat dotfiles-dir "/vendor/org-6.34c/lisp"))
 
